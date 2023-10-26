c565a6eae7f90ebd9affd20ec26eb1f0e180da15
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2023-10-26 18:39:50 -0300

    add : env variables

---
+++ src/lib/firebase.js
@@ -0,0 +1,43 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  limit,
+  getDocs,
+} from "firebase/firestore";
+
+console.log(process.env.API_KEY);
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+};
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+
+export async function getUserWithUsername(username) {
+  const usersRef = collection(db, "users");
+  const q = query(usersRef, where("username", "==", username), limit(1));
+  const userDoc = (await getDocs(q)).docs[0];
+  return userDoc;
+}
+
+export function postToJSON(doc) {
+  const data = doc.data();
+  return {
+    ...data,
+    // Gotcha! firestore timestamp NOT serializable to JSON. Must convert to milliseconds
+    createdAt: data.createdAt.toMillis(),
+    updatedAt: data.updatedAt.toMillis(),
+  };
+}
